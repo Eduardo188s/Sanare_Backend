9abb3428d942b0f9517dbc676576156e7ffa3575
Eduardo188s <eduardodelarosa188@gmail.com>
2025-11-26 20:28:58 -0600

    Deploy28

--- backend/sanare/urls.py
+++ backend/sanare/urls.py
@@ -2,6 +2,10 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import JsonResponse
+
+def backend_root(request):
+    return JsonResponse({"message": "Backend activo", "url": "https://sanarebackend-production.up.railway.app"})
 
 urlpatterns = [
     path('admin/', admin.site.urls),
